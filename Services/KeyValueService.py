5010383dfe636d0aaf86e7eca1637ad685bef909
DhanakTwinkle <dhanak@usc.edu>
2024-03-03 01:45:14 -0800

    Service Class for KeyValue Store
    Service Class for KeyValue Store

---
+++ Services/KeyValueService.py
@@ -0,0 +1,8 @@
+class KeyValueService:
+
+    def __init__(self):
+        pass
+
+    def getAllKeys(self):
+        print("Getting all keys")
+        #raise HTTPException(status_code=404, detail="Item not found")
