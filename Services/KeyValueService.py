c2501aab8762f228f8a216908d5d028f75a228f0
DhanakTwinkle <dhanak@usc.edu>
2024-03-06 11:57:59 -0800

    Service Layer to connect to RedisJSON with Huey tasks
    Service Layer to connect to RedisJSON with Huey tasks

--- Services/KeyValueService.py
+++
@@ -1,8 +0,0 @@
-class KeyValueService:
-
-    def __init__(self):
-        pass
-
-    def getAllKeys(self):
-        print("Getting all keys")
-        #raise HTTPException(status_code=404, detail="Item not found")
